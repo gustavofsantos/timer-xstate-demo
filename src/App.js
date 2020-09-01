1155b608d6c360b3d1ad4712505a12f17540e5ef
Gustavo Santos <gustavofsantos@outlook.com>
2020-09-01 18:57:35 -0300

    Added prettier and setup eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 function App() {
   return (
     <div>
       <h1>Timer</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
